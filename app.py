c4c8a06ac7cfe57f0588396d5e7fe4b3a85f8ac4
Gamfah <89561636+Gamfah@users.noreply.github.com>
2021-09-02 13:34:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello ภฤดา เกิดสม เลขที่22 ห้อง4/5"
+    return "ภฤดา เกิดสม เลขที่22 ห้อง4/5"
 
 @app.route('/api', methods=['GET'])
 def get_api():
