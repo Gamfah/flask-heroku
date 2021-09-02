aeea7e4c7e5722575728a16df40f7d286a88fd22
Gamfah <89561636+Gamfah@users.noreply.github.com>
2021-09-02 10:11:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ def hello():
 @app.route('/api', methods=['GET'])
 def get_api():
     return jsonify(data)
+@app.route('/h')
+@app.route('/hello')
+def hi():
+    return"สวัสดี"
 
 if __name__ == "__main__":
     app.run(debug=False)
